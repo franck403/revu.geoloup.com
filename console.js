95b2b35cd1b8afc5b06337eeee9c1c14a2bd520c
franck403 <79372051+franck403@users.noreply.github.com>
2022-10-26 17:06:45 -0400

    Update console.js

--- console.js
+++ console.js
@@ -1,2 +1,7 @@
+const execSync = require('child_process').execSync;
+// import { execSync } from 'child_process';  // replace ^ if using ES modules
 
-
+const output = execSync('npm install -D tailwindcss', { encoding: 'utf-8' });  // the default is 'buffer'
+console.log('Output was:\n', output);
+const output = execSync('npx tailwindcss init', { encoding: 'utf-8' });  // the default is 'buffer'
+console.log('Output was:\n', output);
