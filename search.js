09707fe9f6fae0e557d527a9db26ce3405c51e31
franck403 <79372051+franck403@users.noreply.github.com>
2022-10-27 20:18:32 -0400

    Update search.js

--- search.js
+++ search.js
@@ -2,7 +2,7 @@
 function search_revu() {
 	let input = document.getElementById('searchbar').value
 	input=input.toLowerCase();
-	let x = document.getElementsByClassName('animals');
+	let x = document.getElementsByClassName('item');
 	
 	for (i = 0; i < x.length; i++) {
 		if (!x[i].innerHTML.toLowerCase().includes(input)) {
