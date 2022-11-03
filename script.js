bcd572fe10effd0e2627eae4090560283ed9b97a
franck403 <79372051+franck403@users.noreply.github.com>
2022-11-03 19:07:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fonction popup() {
+function popup() {
 	let id = document.getElementById("open-popup");
 	let popupWindow = null;
 	let windowSize = 'width=800,height=500,left=0,top=0';
@@ -6,11 +6,11 @@ fonction popup() {
 	id.addEventListener("click", function(event){
 		let url = this.dataset.href;
 		if (popupWindow === null || popupWindow.closed) {
-			popupWindow = window.open(url, 'Login to revu geoloup com', windowSize);
+			popupWindow = window.open(url, 'Logintorevugeoloupcom', windowSize);
 		} else {
 			popupWindow.focus();
 		}
 		event.preventDefault();
 	});
-})();
+}
 /*<button type="button" id="open-popup" data-href="revu.geoloup.com/popup/login">Login</button>
