d6c8ac86fb1c219f5290c6342ef4e1ecd5e26a71
franck403 <79372051+franck403@users.noreply.github.com>
2022-11-03 14:31:04 -0400

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,2 +1,16 @@
+fonction popup() {
+	let id = document.getElementById("open-popup");
+	let popupWindow = null;
+	let windowSize = 'width=800,height=500,left=0,top=0';
 
-
+	id.addEventListener("click", function(event){
+		let url = this.dataset.href;
+		if (popupWindow === null || popupWindow.closed) {
+			popupWindow = window.open(url, 'Logintorevugeoloupcom', windowSize);
+		} else {
+			popupWindow.focus();
+		}
+		event.preventDefault();
+	});
+})();
+
