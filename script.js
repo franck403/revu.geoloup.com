69fca889a206dd2cebfe701f51cccf91c697237c
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-16 10:59:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-$(window).on('load resize',function(){if($(window).width() < 950){window.location = "https://www.google.com"}});
+$(window).on('load resize',function(){if($(window).width() < 950){window.location = "/petit/index.html/"}});
