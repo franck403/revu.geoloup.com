1842c81ff0a8eace97ff06a132e9512fff501991
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-16 06:45:49 -0500

    Update script.js

--- petit/script.js
+++ petit/script.js
@@ -1,2 +1,26 @@
+.rlist {
+  list-style: inside;
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 
-
+.tlist {
+  font-family: 'Trebuchet MS', sans-serif;
+}
+.tlist:link {
+  text-decoration: none;
+}
+
+.tlist:visited {
+  text-decoration: none;
+}
+
+.tlist:hover {
+  text-decoration: underline;
+}
+
+.tlist:active {
+  text-decoration: underline;
+}
