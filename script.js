0c58a7f49cfab7a1846fe51f09bc5749e343a75a
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-16 10:55:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function parseJwt (token) {
-   var base64Url = token.split('.')[1];
-   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-
-  return JSON.parse(jsonPayload);
-}; 
-function handleCredentialResponse(response) {
-  console.log(JSON.stringify(parseJwt(response.credential)));
-    }
+$(window).on('load resize',function(){if($(window).width() < 950){window.location = "https://www.google.com"}});
