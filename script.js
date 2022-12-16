53caba23f9480d0224ca5b5594e5580c660040eb
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-16 11:02:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-$(window).on('load resize',function(){if($(window).width() < 950){window.location = "/petit/index.html/"}});
+   function redirect() {
+   if (screen.width <= 950) {
+      window.location = "/petit/index.html";
+   }
