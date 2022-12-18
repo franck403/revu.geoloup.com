a64f077765fd1ced7755046ee4820bdfd44b68d3
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2022-12-18 17:04:04 -0500

    test

--- loader.js
+++ loader.js
@@ -1,1 +1,2 @@
-document.getElementById("revue").src=new URLSearchParams(window.location.search).get('load');
+
+    document.getElementById("revue").src=new URLSearchParams(window.location.search).get('load');
