f2dd488ca32b0c03a2376bfbf59d452d912ab86d
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-18 21:02:08 +0000

    add the url parm geter

---
+++ loader.js
@@ -0,0 +1,8 @@
+const queryString = window.location.search;
+console.log(queryString);
+
+const urlParams = new URLSearchParams(queryString);
+
+const newUser = urlParams.get('load')
+console.log(newUser);
+// empty string
