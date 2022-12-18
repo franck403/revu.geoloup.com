014dcf0e67e60f86893a866c47312629c44212a8
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-18 21:35:23 +0000

    add function laod

--- loader.js
+++ loader.js
@@ -1,2 +1,5 @@
 document.getElementById("revue").src=URLSearchParams.window.location.search.get('load');
 
+function load() {
+    document.getElementById("revue").src=URLSearchParams.window.location.search.get('load');
+}
