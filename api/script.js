6af574bebab6a544ecb9182f81d92171fcb8d26b
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-18 22:42:13 +0000

    delete

--- api/script.js
+++
@@ -1,8 +0,0 @@
-csrf_token_cookie = self.request.cookies.get('g_csrf_token')
-if not csrf_token_cookie:
-    webapp2.abort(400, 'No CSRF token in Cookie.')
-csrf_token_body = self.request.get('g_csrf_token')
-if not csrf_token_body:
-    webapp2.abort(400, 'No CSRF token in post body.')
-if csrf_token_cookie != csrf_token_body:
-    webapp2.abort(400, 'Failed to verify double submit cookie.')
