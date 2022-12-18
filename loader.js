1d32aff7df00cbb6fe193ba8dc4500fe58b3060e
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-18 21:37:07 +0000

    add the old methods

--- loader.js
+++ loader.js
@@ -1,5 +1,14 @@
 document.getElementById("revue").src=URLSearchParams.window.location.search.get('load');
 
-function load() {
-    document.getElementById("revue").src=URLSearchParams.window.location.search.get('load');
+function load() {    
+    const queryString = window.location.search;
+    console.log(queryString);
+
+    const urlParams = new URLSearchParams(queryString);
+
+    const Revue = urlParams.get('load')
+    console.log(Revue);
+
+    document.getElementById("revue").src=Revue;
+    document.Revue.src=Revue;
 }
