c566c17e79472fae3523af9f4aa33f49672c57b7
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2022-12-18 16:17:31 -0500

    variable

--- loader.js
+++ loader.js
@@ -6,5 +6,5 @@ const urlParams = new URLSearchParams(queryString);
 const Revue = urlParams.get('load')
 console.log(Revue);
 
-document.getElementById("revue").src="(Revue)";
+document.getElementById("revue").src=(Revue);
 
