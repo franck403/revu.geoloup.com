8927ea60cc11d38c473ff73537704c675641cd0b
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2022-12-18 16:39:39 -0500

    fix

--- loader.js
+++ loader.js
@@ -1,14 +1,3 @@
-document.getElementById("revue").src=URLSearchParams.(window.location.search).get('load');
-
-function load() {    
-    const queryString = window.location.search;
-    console.log(queryString);
-
-    const urlParams = new URLSearchParams(queryString);
-
-    const Revue = urlParams.get('load')
-    console.log(Revue);
-
-    document.getElementById("revue").src=Revue;
-    document.Revue.src=Revue;
+function load(){
+    document.getElementById("revue").src=URLSearchParams.(window.location.search).get('load');
 }
