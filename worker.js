afee97a3de523abce022e9004cb191bd016f6cff
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-19 11:04:06 +0000

    add the second and min timer

--- worker.js
+++ worker.js
@@ -1,7 +1,12 @@
 var i = 0;
 function list() {
   i = i + 1;
-  var revu = i
+  if (i < 61) {
+    var revu = i;
+  }
+  else {
+    revu = i / 60;
+  }
   postMessage(revu);
   setTimeout("list()",100000000000000000000000000000000000000000000);
 }
