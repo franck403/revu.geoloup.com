00bb3eacaa0ece45e236be96a96fe9fb69c853da
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-19 11:44:31 +0000

    add sleep function

--- worker.js
+++ worker.js
@@ -1,14 +1,18 @@
 function fmtMSS(s){return(s-(s%=60))/60+(9<s?':':':0')+s}
 
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 var i = 0;
 function list() {
   i = i + 1;
-  if (i < 61) {
-    var revu = i;
-  }
-  else {
-    revu = fmtMSS(i);
-  }
+  sleep(60000)
+  revu = fmtMSS(i);
   postMessage(revu);
   setTimeout("list()",100000000000000000000000000000000000000000000);
 }
