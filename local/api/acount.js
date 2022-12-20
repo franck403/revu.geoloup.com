492e215e24671d90a658d7f6d2d7dc73176b5b65
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-20 11:33:12 +0000

    add the new code

--- local/api/acount.js
+++ local/api/acount.js
@@ -1,19 +1,19 @@
 var jwt = localStorage.getItem("jwt");
 if (jwt == null) {
-  window.location.href = "./login.html";
+  window.location.href = 'https://revu.geoloup.com/local/api/login.html'
 }
 
 function loadUser() {
   const xhttp = new XMLHttpRequest();
   xhttp.open("GET", "https://www.mecallapi.com/api/auth/user");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
+  xhttp.setRequestHeader("Authorization", "Bearer "+jwt);
   xhttp.send();
-  xhttp.onreadystatechange = function () {
+  xhttp.onreadystatechange = function() {
     if (this.readyState == 4) {
       const objects = JSON.parse(this.responseText);
       if (objects["status"] == "ok") {
-        const user = objects["user"];
+        const user = objects["user"]
         document.getElementById("fname").innerHTML = user["fname"];
         document.getElementById("avatar").src = user["avatar"];
         document.getElementById("username").innerHTML = user["username"];
@@ -26,5 +26,5 @@ loadUser();
 
 function logout() {
   localStorage.removeItem("jwt");
-  window.location.href = "./login.html";
+  window.location.href = 'https://revu.geoloup.com/local/api//login.html'
 }
--- local/api/verfy.js
+++ local/api/verfy.js
@@ -1,6 +1,6 @@
 var jwt = localStorage.getItem("jwt");
 if (jwt != null) {
-  window.location.href = "./index.html";
+  window.location.href = 'https://revu.geoloup.com/local/api/account.html'
 }
 
 function login() {
@@ -10,32 +10,30 @@ function login() {
   const xhttp = new XMLHttpRequest();
   xhttp.open("POST", "https://www.mecallapi.com/api/login");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhttp.send(
-    JSON.stringify({
-      username: username,
-      password: password,
-    })
-  );
+  xhttp.send(JSON.stringify({
+    "username": username,
+    "password": password
+  }));
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
       const objects = JSON.parse(this.responseText);
       console.log(objects);
-      if (objects["status"] == "ok") {
-        localStorage.setItem("jwt", objects["accessToken"]);
+      if (objects['status'] == 'ok') {
+        localStorage.setItem("jwt", objects['accessToken']);
         Swal.fire({
-          text: objects["message"],
-          icon: "success",
-          confirmButtonText: "OK",
+          text: objects['message'],
+          icon: 'success',
+          confirmButtonText: 'OK'
         }).then((result) => {
           if (result.isConfirmed) {
-            window.location.href = "./index.html";
+            window.location.href = 'https://revu.geoloup.com/local/api/account.html';
           }
         });
       } else {
         Swal.fire({
-          text: objects["message"],
-          icon: "error",
-          confirmButtonText: "OK",
+          text: objects['message'],
+          icon: 'error',
+          confirmButtonText: 'OK'
         });
       }
     }
