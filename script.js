16755710ede68721ad612013a29f929ff2888152
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-20 14:41:39 +0000

    codespace is bug

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ function redirect() {
 if (screen.width < '1510px'){
    window.location.replace("https://revu.geoloup.com/petit/index.html");
 }
-            
+
+var urlparm = new URLSearchParams(window.location.search).has('load')
+var cookierevu = document.cookie.valueOf("lastr")
+var cookielast = cookierevu.replace('lastr=','')
+document.cookie = lastr;
+document.cookie = lastr;
+document.getElementById("revue").src="error.html";
+var lastrevu = document.cookie.valueOf("lastr")
+var laster = lastrevu.replace('lastr=','')
+document.getElementById("revue").src=laster;
