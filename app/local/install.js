6cd3a9c8e645771c71517c2ce8fb4172b892b035
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-23 16:14:32 +0000

    add pwa support

--- app/local/install.js
+++ app/local/install.js
@@ -1,6 +1,6 @@
 // Files to cache
-const cacheName = 'js13kPWA-v1';
-const appShellFiles = [
+const cacheName = 'revu-Geoloup-V1';
+const assets = [
   '/app/',
   '/app/view.html',
   '/app/local/main.min.js',
@@ -8,19 +8,13 @@ const appShellFiles = [
   '/search.js',
   '/app/style.css',
   '/app/blog.css',
-  'https://revu.geoloup.com/image/maison.png',
+  '/image/maison.png',
 ];
 
-// Fetching content using Service Worker
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) return r;
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
-});
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(cachename).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
