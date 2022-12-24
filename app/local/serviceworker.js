1e752fd9b343a6101bdfc0a36b5e0ca7e9e7ac71
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-24 16:32:57 +0000

    because

--- app/local/serviceworker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
