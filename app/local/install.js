10d11491378cb25d83c26849984812ee145d1d6a
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-24 15:53:55 +0000

    test new code

--- app/local/install.js
+++ app/local/install.js
@@ -11,10 +11,26 @@ const assets = [
   '/image/maison.png',
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(cachename).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(assets);
+  })());
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
