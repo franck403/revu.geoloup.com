a3b2d6fe81fece5c11e03d1ddd58ec7d27ec41df
franck403 <79372051+franck403@users.noreply.github.com>
2022-12-28 14:30:53 +0000

    fix bug and desactivated the ofline mode

--- app/sw.js
+++ app/sw.js
@@ -22,16 +22,6 @@ const file = [
 ];
 
 
-// Installing Service Worker
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(file);
-  })());
-});
-
 // Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
   e.respondWith((async () => {
