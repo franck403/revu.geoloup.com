ab85d9327328b0126dc69825958fd763ff40e952
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-05 23:45:06 +0000

    change ting

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ function redirect() {
 if (screen.width < '1510px'){
    window.location.replace("https://revu.geoloup.com/petit/index.html");
 }
+document.getElementById('last').innerHTML = "<a href='nouvelan.html'>Nouvel Ans</a>";
